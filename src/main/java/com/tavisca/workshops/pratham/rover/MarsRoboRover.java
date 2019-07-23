9eca50d7bf44b7e8273f85396dbac912a6eb6616
vvinod <vvinod@tavisca.in>
2019-07-23 11:23:30 +0530

    Completed encapsulation

--- src/main/java/com/tavisca/workshops/pratham/rover/MarsRoboRover.java
+++ src/main/java/com/tavisca/workshops/pratham/rover/MarsRoboRover.java
@@ -1,96 +1,23 @@
 package com.tavisca.workshops.pratham.rover;
 
 public class MarsRoboRover {
-	private static int y = 0;
-	private static int x = 0;
-	private static String dir = "";
-
 	public static void main(String[] args) {
 
 		String currentPosition = "3 3 E";
 		String commands = "MMRMMRMRRM";
 
+		Rover rover= new Rover();
 		String[] positions = currentPosition.split(" ");
-		x = Integer.valueOf(positions[0]);
-		y = Integer.valueOf(positions[1]);
-		dir = positions[2];
+		rover.setXcoordinate(Integer.valueOf(positions[0]));
+		rover.setYcoordinate(Integer.valueOf(positions[1]));
+		rover.setDirection(positions[2]);
 
 		for (char command : commands.toCharArray()) {
-			rove(command);
+			rover.roverMove(command, rover);
 		}
 
 		System.out.println("currentPosition..." + currentPosition);
 		System.out.println("commands..." + commands);
-		System.out.println("newPosition..." + x + " " + y + " " + dir);
+		System.out.println("newPosition..." + rover.getXcoordinate() + " " +rover.getYcoordinate() + " " + rover.getDirection());
 	}
-
-	private static void rove(char command) {
-		if (dir.equalsIgnoreCase("N")) {
-			FacingNorth(command);
-		} else if (dir.equalsIgnoreCase("E")) {
-			FacingEast(command);
-		} else if (dir.equalsIgnoreCase("S")) {
-			FacingSouth(command);
-		} else if (dir.equalsIgnoreCase("W")) {
-			FacingWest(command);
-		}
-	}
-
-	public static void FacingNorth(char command)
-	{
-		switch (command) {
-			case 'L':
-				dir = "W";
-				break;
-			case 'R':
-				dir = "E";
-				break;
-			case 'M':
-				y++;
-				break;
-		}
-	}
-	public static void FacingEast(char command)
-	{
-		switch (command) {
-			case 'L':
-				dir = "N";
-				break;
-			case 'R':
-				dir = "S";
-				break;
-			case 'M':
-				x++;
-				break;
-		}
-	}
-	public static void FacingSouth(char command)
-	{
-		switch (command) {
-			case 'L':
-				dir = "E";
-				break;
-			case 'R':
-				dir = "W";
-				break;
-			case 'M':
-				y--;
-				break;
-		}
-	}
-	public static void FacingWest(char command)
-	{
-		switch (command) {
-			case 'L':
-				dir = "S";
-				break;
-			case 'R':
-				dir = "N";
-				break;
-			case 'M':
-				x--;
-				break;
-		}
-	}
-
 }
--- src/main/java/com/tavisca/workshops/pratham/rover/Rover.java
+++ src/main/java/com/tavisca/workshops/pratham/rover/Rover.java
@@ -1,4 +1,98 @@
 package com.tavisca.workshops.pratham.rover;
 
 public class Rover {
+     private int y = 0;
+     private int x = 0;
+     private String dir = "";
+     public void setXcoordinate(int coordinatex)
+     {
+         this.x=coordinatex;
+     }
+     public void setYcoordinate(int coordinatey)
+     {
+        this.y=coordinatey;
+     }
+     public void setDirection(String direction)
+     {
+        this.dir=direction;
+     }
+     public String getDirection()
+     {
+        return this.dir;
+     }
+     public int getXcoordinate()
+     {
+       return this.x;
+     }
+     public int getYcoordinate()
+     {
+       return this.y;
+     }
+     public  void FacingNorth(char command)
+     {
+        switch (command) {
+            case 'L':
+                dir = "W";
+                break;
+            case 'R':
+                dir = "E";
+                break;
+            case 'M':
+                y++;
+                break;
+        }
+     }
+     public  void FacingEast(char command)
+     {
+        switch (command) {
+            case 'L':
+                dir = "N";
+                break;
+            case 'R':
+                dir = "S";
+                break;
+            case 'M':
+                x++;
+                break;
+        }
+     }
+     public  void FacingSouth(char command)
+     {
+        switch (command) {
+            case 'L':
+                dir = "E";
+                break;
+            case 'R':
+                dir = "W";
+                break;
+            case 'M':
+                y--;
+                break;
+        }
+     }
+     public  void FacingWest(char command)
+     {
+        switch (command) {
+            case 'L':
+                dir = "S";
+                break;
+            case 'R':
+                dir = "N";
+                break;
+            case 'M':
+                x--;
+                break;
+        }
+     }
+     public void roverMove(char command, Rover rover) {
+        if (rover.getDirection().equalsIgnoreCase("N")) {
+            rover.FacingNorth(command);
+        } else if (rover.getDirection().equalsIgnoreCase("E")) {
+            rover.FacingEast(command);
+        } else if (rover.getDirection().equalsIgnoreCase("S")) {
+            rover.FacingSouth(command);
+        } else if (rover.getDirection().equalsIgnoreCase("W")) {
+            rover.FacingWest(command);
+        }
+     }
 }
